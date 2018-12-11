47a9f4fda2c1bd17d7df1dd62fbcf157047b3633
Daniel Wilson <2056929+dannosliwcd@users.noreply.github.com>
2018-12-11 14:19:32 -0500

    Add most common county selection

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,5 +1,6 @@
-#include <CountyRecord.h>
-#include <PointFinder.h>
+#include "CountyRecord.h"
+#include "PointFinder.h"
+#include "MaxHeap.h"
 #include <CLI11.hpp>
 #include <iostream>
 #include <vector>
@@ -12,9 +13,13 @@
 #include <errno.h>
 #include <exception>
 #include <chrono>
+#include <tuple>
+
+static const unsigned int COUNTY_KNN_COUNT(5);
 
 int main(int argc, char** argv)
 {
+	auto start = std::chrono::steady_clock::now();
 	static const std::string METHOD_LINEAR("linear");
 	static const std::string METHOD_QUADTREE("quadtree");
 	static const std::string METHOD_KD_TREE("kdtree");
@@ -92,7 +97,7 @@ int main(int argc, char** argv)
 	auto pointFinder = PointFinder::Make(method, records);
 	auto buildEnd = std::chrono::steady_clock::now();
 	auto buildElapsed = std::chrono::duration<float>(buildEnd - buildStart);
-	std::cout << "Time to build the index: " << buildElapsed.count() << " s" << std::endl;
+	std::cout << "Time to build the index: " << buildElapsed.count() << " s\n" << std::endl;
 	for (const auto& query : queries)
 	{
 		auto delimeterIndex = query.find(":");
@@ -127,7 +132,53 @@ int main(int argc, char** argv)
 				<< point.m_latitude << ", " << (point.m_longitude > 180 ? point.m_longitude - 360.0f : point.m_longitude) << ")\n";
 		}
 
-		std::cout << "Time to calculate nearest neighbors: " << knnElapsed.count() << " s" << std::endl;
+		std::cout << "Time to calculate nearest neighbors: " << knnElapsed.count() << " s\n" << std::endl;
+
+		// Perform the mode county lookup separately instead of building it in to the requested knn search
+		// so we can measure the time for k less than the county lookup count.
+		auto countyStart = std::chrono::steady_clock::now();
+		auto countyPoints = pointFinder->FindNearest(
+				latitude,
+			       	longitude < 0 ? longitude + 360.0f : longitude,
+			       	COUNTY_KNN_COUNT);
+		std::vector<std::pair<CountyRecord, unsigned>> countyCounts;
+		while (!countyPoints.empty())
+		{
+			bool found = false;
+			const auto& countyPoint = countyPoints.back();
+			for (auto& countyCount : countyCounts)
+			{
+				if (countyCount.first.m_county == countyPoint.m_county
+						&& countyCount.first.m_state == countyPoint.m_state)
+				{
+					countyCount.second += 1;
+					found = true;
+				}
+			}
+			if (!found)
+			{
+				countyCounts.emplace_back(countyPoint, 1);
+			}
+			countyPoints.pop_back();
+		}
+
+		MaxHeap<unsigned, CountyRecord> orderedCountyCounts(countyCounts.size());
+		for (const auto& countyCount : countyCounts)
+		{
+			orderedCountyCounts.Insert(countyCount.second, countyCount.first);
+		}
+
+		const auto& selectedCounty = orderedCountyCounts.GetMax().e;
+		std::cout << "Point is most likely in \"" << selectedCounty.m_county << "\" county, \""
+		       	<< selectedCounty.m_state << "\" state" << std::endl;
+
+		auto countyEnd = std::chrono::steady_clock::now();
+		auto countyElapsed = std::chrono::duration<float>(countyEnd - countyStart);
+		std::cout << "Time to select county: " << knnElapsed.count() << " s\n\n" << std::endl;
+		
+		auto end = std::chrono::steady_clock::now();
+		auto elapsed = std::chrono::duration<float>(end - start);
+		std::cout << "Total time in main(): " << elapsed.count() << " s" << std::endl;
 		std::cout << std::flush;
 	}
 
