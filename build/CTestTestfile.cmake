9deb3f412163032ec40c04c91cdde673c05bede9
brianrayxu <brianxu@bu.edu>
2018-12-10 20:20:58 -0500

    Adding first set of components

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/Brian/Desktop/EC504/PointQuery
+# Build directory: /cygdrive/c/Users/Brian/Desktop/EC504/PointQuery/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(thirdparty/cli11)
+subdirs(thirdparty/catch)
+subdirs(src)
+subdirs(test)
